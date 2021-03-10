020d89df6ab480b37b20fb0e677dc5957587e56f
蒋晨成 <jccdragon@icloud.com>
2021-03-10 18:05:00 +0800

    update

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module geak
 go 1.16
 
 require (
-	github.com/golang/protobuf v1.4.2
+	gitee.com/jlab/biz v1.0.2
+	github.com/golang/protobuf v1.4.2 // indirect
 	google.golang.org/grpc v1.36.0
-	google.golang.org/protobuf v1.25.0
+	google.golang.org/protobuf v1.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"context"
-	"errors"
-	login "geak/model"
+	"gitee.com/jlab/biz/login"
+	"gitee.com/jlab/biz/user"
+	//login "geak/model"
 	"google.golang.org/grpc"
 	"log"
 	"net"
@@ -13,23 +14,28 @@ type server struct {
 	login.UnimplementedLoginServer
 }
 
-func (s *server) Login(ctx context.Context, in *login.LoginRequest) (*login.LoginResponse, error) {
-	log.Printf("Received: %v", in.GetName())
-	return nil,errors.New("1111")
+func (s *server) Login(ctx context.Context, in *login.NormalLoginRequest) (*login.NormalLoginResponse, error) {
+	log.Printf("Received Moble: %v password:%v", in.GetMobile(),in.GetPassword())
+	return &login.NormalLoginResponse{
+		Guid:"100",
+		UserInfo:&user.BasicProfile{
+			Guid:"100",
+			Username:"Admin",
+			UserID:100,
+		},
+	},nil
 }
 
 func main()  {
 
-	lis,err := net.Listen("tcp", ":18080")
+	lis,err := net.Listen("tcp", ":443")
 	if err != nil {
 		log.Fatal("failed to listem: %v",err)
 	}
 	s := grpc.NewServer()
 	login.RegisterLoginServer(s, &server{})
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-
-
-
 }
